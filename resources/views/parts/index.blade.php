b0cde6e556fd9b150f80d1ace97ebd061e8146b4
Jefferson J. Ortega <jeffersonj-o@hotmail.com>
2020-10-03 21:44:44 -0500

    four tables for parts

--- resources/views/parts/index.blade.php
+++ resources/views/parts/index.blade.php
@@ -36,16 +36,105 @@
                     @endforeach
                 </tbody>
             </table>
-            @if($errors->any())
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach($errors->all() as $error)
-                    <h4><i class="icon fa fa-ban"></i> Alert!</h4>
-                    <li>{{ $error }}</li>
+        </div>
+        <div class="col-md-6 mx-left">
+            <h2 style="font-weight: 700">Listado de Roles</h2>
+            @include('roles.modal')
+            <table class="table table-hover table-bordered text-center">
+                <thead class="thead-dark">
+                    <tr>
+                        <th hidden scope="col">ID</th>
+                        <th scope="col">
+                            <i class="fas fa-users-cog"></i>
+                        </th>
+                        <th scope="col"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($types as $type)
+                    <tr>
+                        <th>
+                            <h4>{{ $type->name }}
+                        </th>
+                        </h4>
+                        <th>
+                            <form action="" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn bg-gradient-danger btn-sm"><i
+                                        class="fas fa-trash-alt"></i></button>
+                            </form>
+                        </th>
+                    </tr>
                     @endforeach
-                </ul>
-            </div>
-            @endif
+                </tbody>
+            </table>
+        </div>
+        <div class="col-md-6 mx-right">
+            <h2 style="font-weight: 700">Listado de Roles</h2>
+            @include('roles.modal')
+            <table class="table table-hover table-bordered text-center">
+                <thead class="thead-dark">
+                    <tr>
+                        <th hidden scope="col">ID</th>
+                        <th scope="col">
+                            <i class="fas fa-users-cog"></i>
+                        </th>
+                        <th scope="col"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($types as $type)
+                    <tr>
+                        <th>
+                            <h4>{{ $type->name }}
+                        </th>
+                        </h4>
+                        <th>
+                            <form action="" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn bg-gradient-danger btn-sm"><i
+                                        class="fas fa-trash-alt"></i></button>
+                            </form>
+                        </th>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        <div class="col-md-6 mx-left">
+            <h2 style="font-weight: 700">Listado de Roles</h2>
+            @include('roles.modal')
+            <table class="table table-hover table-bordered text-center">
+                <thead class="thead-dark">
+                    <tr>
+                        <th hidden scope="col">ID</th>
+                        <th scope="col">
+                            <i class="fas fa-users-cog"></i>
+                        </th>
+                        <th scope="col"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($types as $type)
+                    <tr>
+                        <th>
+                            <h4>{{ $type->name }}
+                        </th>
+                        </h4>
+                        <th>
+                            <form action="" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn bg-gradient-danger btn-sm"><i
+                                        class="fas fa-trash-alt"></i></button>
+                            </form>
+                        </th>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
